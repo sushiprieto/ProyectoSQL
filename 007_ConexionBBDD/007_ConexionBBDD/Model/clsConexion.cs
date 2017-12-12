ece9672c63fd8ef34b4abf4209b86a6f0c1b7198
Carlos Prieto <carlosprietotrabajo@gmail.com>
2017-12-12 10:40:38 +0100

    Subidita wapa

---
+++ 007_ConexionBBDD/007_ConexionBBDD/Model/clsConexion.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _007_ConexionBBDD.Model
+{
+    public class clsConexion
+    {
+
+        //Atributos
+        public String host { get; set; }
+        public String dataBase { get; set; }
+        public String user { get; set; }
+        public String pass { get; set; }
+
+        public clsConexion()
+        {
+
+            this.host = "localhost";
+            this.dataBase = "colegio";
+            this.user = "root";
+            this.pass = "root";
+
+        }
+
+        public clsConexion(String host, String dataBase, String user, String pass)
+        {
+            this.dataBase = dataBase;
+            this.user = user;
+            this.pass = pass;
+            this.host = host;
+        }
+
+        public MySqlConnection getConnection()
+        {
+            MySqlConnection connection = new MySqlConnection();
+            try
+            {
+
+                connection.ConnectionString = string.Format("server={0};database={1};uid={2};pwd={3};", host, dataBase, user, pass);
+                
+
+                connection.Open();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return connection;
+
+        }
+
+        public void closeConnection(ref MySqlConnection connection)
+        {
+            try
+            {
+                connection.Close();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+    }
+}
---
+++ 007_ConexionBBDD/007_ConexionBBDD/View/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _007_ConexionBBDD
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoadDataGrid_Click(object sender, RoutedEventArgs e)
+        {
+            string password = "root";
+            string connStr =
+                "server=localhost;user=root;database=colegio;port=3306;password=" + password;
+            MySqlConnection conn = new MySqlConnection(connStr);
+            conn.Open();
+            MySqlCommand cmd = new MySqlCommand("SELECT AlumnoID,Nombre,Apellidos,Curso,Sexo,NotaExamen FROM alumno", conn);
+            MySqlDataAdapter adp = new MySqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            adp.Fill(ds, "LoadDataBinding");
+            dataGridCustomers.DataContext = ds;
+            conn.Close();
+        }
+    }
+}
